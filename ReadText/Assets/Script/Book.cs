7d247700c85522db9d0c9953f8fc7c9a9fa710be
Iandlyric19831 <chengyongxin1983@qq.com>
2016-05-10 09:11:56 +0800

    delete redoundant files

---
+++ ReadText/Assets/Script/Book.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Book  {
+
+	public string text;
+
+
+	// Use this for initialization
+	public Book () {
+	
+	}
+
+	public void Init(string pathName)
+	{
+		TextAsset txt = Resources.Load(pathName) as TextAsset; 	
+		if (txt != null)
+		{
+			text = txt.text;
+		}
+	}
+
+
+
+
+}
